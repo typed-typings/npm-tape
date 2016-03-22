218dee4c78f5758136181821b41c4995d20721a1
Blake Embrey <hello@blakeembrey.com>
2016-03-22 16:46:21 -0700

    Update type definitions

--- test.ts
+++ test.ts
@@ -1,7 +1,8 @@
-/// <reference path="bundle.d.ts" />
+/// <reference path="bundle/main.d.ts" />
 import test = require('tape');
 
 test('a test', (t) => {
   t.plan(1);
+  t.comment('test');
   t.assert(1 === 1);
 });
